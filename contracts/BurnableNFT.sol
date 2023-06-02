30ce5175a0e3baf6114c74deab24a10920ebdd08
kakiuchis <kakiuchis@gmail.com>
2023-06-02 17:30:24 +0900

    Add burn pause functions

---
+++ contracts/BurnableNFT.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Strings.sol";
+
+contract BurnableNFT is ERC721Burnable, ERC721URIStorage, Ownable {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    event TokenURIChanged(address indexed sender, uint256 indexed tokenID, string uri);
+
+    constructor() ERC721("BurnableNFT", "BURN") {
+    }
+
+    function nftMint() public onlyOwner {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(_msgSender(), newTokenId);
+
+        string memory jsonFile = string(abi.encodePacked('metadata', Strings.toString(newTokenId), '.json'));
+        _setTokenURI(newTokenId, jsonFile);
+        emit TokenURIChanged(_msgSender(), newTokenId, jsonFile);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafybeigyod7ldrnytkzrw45gw2tjksdct6qaxnsc7jdihegpnk2kskpt7a/";
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
